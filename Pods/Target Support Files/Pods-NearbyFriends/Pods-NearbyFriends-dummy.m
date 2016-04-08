11bfa2a5575210ffcdedd60c259e202ee9103ce5
Marco Tabacchino <marcoiano@hotmail.it>
2016-04-08 15:11:01 +0200

    pod install

---
+++ Pods/Target Support Files/Pods-NearbyFriends/Pods-NearbyFriends-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NearbyFriends : NSObject
+@end
+@implementation PodsDummy_Pods_NearbyFriends
+@end
---
+++ Pods/Target Support Files/Pods-NearbyFriends/Pods-NearbyFriends-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_NearbyFriendsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_NearbyFriendsVersionString[];
+
